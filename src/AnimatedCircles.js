ee2f6cda93d355f676509a604a46dec7cb02cd34
ikalwani <ikalwani@umich.edu>
2024-07-25 13:37:34 +0100

    Finished landing page, working on login page + auth

---
+++ src/AnimatedCircles.js
@@ -0,0 +1,16 @@
+// src/AnimatedCircles.js
+import React from "react";
+import "./AnimatedCircles.css";
+
+const AnimatedCircles = () => {
+  return (
+    <div className="circles">
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
+      <div className="circle circle3"></div>
+      <div className="circle circle4"></div>
+    </div>
+  );
+};
+
+export default AnimatedCircles;
