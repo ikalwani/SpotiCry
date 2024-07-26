58360e29cce7c7d0d5b87ec2d225c3563f02e86b
ikalwani <ikalwani@umich.edu>
2024-07-26 01:57:39 +0100

    made dashboard responsive

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,7 +6,6 @@ const Dashboard = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Logout logic here
     navigate("/");
   };
 
@@ -19,33 +18,24 @@ const Dashboard = () => {
         <div className="header-item" onClick={() => navigate("/visualize")}>
           Visualize
         </div>
+        <button className="logout-button-header" onClick={handleLogout}>
+          Logout
+        </button>
       </header>
 
       <main className="dashboard-main">
         <h2>Welcome to Your Dashboard</h2>
         <p>
-          Need to log your tears? Fill the form below and we will take care of the rest
+          Need to log your tears? Fill the form below and we will take care of
+          the rest.
         </p>
         <div className="google-form-container">
-          {/* Embed Google Form */}
-          {/* <iframe
-            src="https://docs.google.com/forms/d/e/YOUR_GOOGLE_FORM_ID/viewform?embedded=true"
-            width="640"
-            height="800"
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSensxeLgh36JOTNZRZyKplrDhRrDYIAxo6mzHWqBPP9x9MllQ/viewform?embedded=true"
             frameBorder="0"
             marginHeight="0"
             marginWidth="0"
             title="Google Form"
-          >
-            Loading…
-          </iframe> */}
-          <iframe
-            src="https://docs.google.com/forms/d/e/1FAIpQLSensxeLgh36JOTNZRZyKplrDhRrDYIAxo6mzHWqBPP9x9MllQ/viewform?embedded=true"
-            width="640"
-            height="3066"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
           >
             Loading…
           </iframe>
@@ -55,10 +45,6 @@ const Dashboard = () => {
       <footer className="dashboard-footer">
         <p>&copy; 2024 SpotiCry. All rights reserved.</p>
       </footer>
-
-      <button className="logout-button" onClick={handleLogout}>
-        Logout
-      </button>
     </div>
   );
 };
